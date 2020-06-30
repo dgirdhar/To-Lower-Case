20e0e65259337246931193a81d591652ba90ce62
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-06-30 19:30:42 -0400

    Create main.cpp
    Initial Checkin.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.size(); ++i) {
+            if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = (int)str[i] + 32;
+            }
+        }
+        
+        return str;
+    }
+};
